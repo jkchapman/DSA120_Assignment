50324d83178a00f03aec167f8e15d4993a3ffbdc
Jacob Chapman <chapman.j94@gmail.com>
2014-10-28 13:11:38 +0800

    Added menu and user input

--- StudentSearch.java
+++ StudentSearch.java
@@ -6,14 +6,28 @@
 
 import io.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class StudentSearch
 {
 
 	public static void main( String[] args)
 	{
-		System.out.println("Test");
+		Scanner input;
+		int selection;
+
+		input = new Scanner(System.in);
+
+		System.out.println("(1)\t - Load Students");
+		System.out.println("(2)\t - Search for Single Student");
+		System.out.println("(3)\t - Search for List of Students");
+		System.out.println("(4)\t - Save Matches");
+		System.out.println("(5)\t - Quit");
+		System.out.print("Choice:>");
+
+		selection = Integer.parseInt(input.nextLine());
+		System.out.printf("%d\n", selection);
+
 		System.exit(0);
 	}
-	
 }
