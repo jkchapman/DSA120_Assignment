2cfb79939b4929ae97d9e388980e178581613c02
Jacob Chapman <chapman.j94@gmail.com>
2014-10-28 18:45:31 +0800

    Stident class created with classfields, def constructor and some stub methods.

---
+++ Student.java
@@ -0,0 +1,26 @@
+import io.*;
+import java.io.*;
+
+public class Student
+{
+
+	//class fields
+	private int studentNo;
+	private String name;
+
+	//default constructor
+	public Student()
+	{
+		studentNo = -1;
+		name = null;
+	}
+
+	//alt constructor
+	public Student( int studentNo, String name)
+	{}
+
+	//name setter
+	public void setName( String name)
+	{}
+
+}
