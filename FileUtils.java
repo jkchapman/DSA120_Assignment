2564adf89545b98aec810abfdf1c0a6226aeff5c
Jacob Chapman <chapman.j94@gmail.com>
2014-10-28 21:25:57 +0800

    FileUtils class created - Contains method to fill student array from file.

---
+++ FileUtils.java
@@ -0,0 +1,51 @@
+import io.*;
+import java.io.*;
+
+public class FileUtils
+{
+	//opens and loops through file, fills array of students, returns number read
+	//reference lecture 2, slide 25
+	public static int fillFromFile( Student[] studentArray, String inputFile)
+	{
+		FileInputStream fileStrm = null;
+		InputStreamReader rdr;
+		BufferedReader bufRdr;
+		Student thisStudent;
+		int num;
+		String line;
+		num = 0;
+		try
+		{
+			fileStrm = new FileInputStream( inputFile);
+			rdr = new InputStreamReader( fileStrm);
+			bufRdr = new BufferedReader(rdr);
+
+			line = bufRdr.readLine();
+			while( line != null)
+			{
+				num++;
+				thisStudent = new Student( line);
+				studentArray[num - 1] = thisStudent;
+				line = bufRdr.readLine();
+			}
+			fileStrm.close();
+		}
+		catch(IOException e)
+		{
+			if(fileStrm != null)
+			{
+				try
+				{
+					fileStrm.close();
+				}
+				catch (IOException e2)
+				{}
+			}
+			System.out.println("Error in processing file: " + e.getMessage());
+		}
+
+		return num;
+		
+	}
+
+}
