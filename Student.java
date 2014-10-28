f44decb471a515687a5c1feee8dbde2690613495
Jacob Chapman <chapman.j94@gmail.com>
2014-10-28 21:01:42 +0800

    Added constructor using csv row

--- Student.java
+++ Student.java
@@ -17,10 +17,32 @@ public class Student
 
 	//alt constructor
 	public Student( int studentNo, String name)
-	{}
+	{
+		this.studentNo = studentNo;
+		setName( name);
+	}
+
+	//alt constructor taking a row from csv file
+	public Student( String row)
+	{
+
+		String rowValues[] = row.split(",");
+		studentNo = Integer.parseInt(rowValues[0]);
+		setName( rowValues[1]);
+
+	}
 
 	//name setter
 	public void setName( String name)
-	{}
+	{
+		if(name == null || name.equals(""))
+		{
+			throw new IllegalArgumentException("Name must not be blank!");
+		}
+		else
+		{
+			this.name = name;
+		}
+	}
 
 }
