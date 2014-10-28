03630eea0548d3c954ef6a9371a4c4946f4c05e7
Jacob Chapman <chapman.j94@gmail.com>
2014-10-28 09:42:06 +0800

    Main class added

---
+++ StudentSearch.java
@@ -0,0 +1,19 @@
+/*
+	Jacob Chapman
+	17779407
+	28/10/2014
+*/
+
+import io.*;
+import java.io.*;
+
+public class StudentSearch
+{
+
+	public static void main( String[] args)
+	{
+		System.out.println("Test");
+		System.exit(0);
+	}
+	
+}
