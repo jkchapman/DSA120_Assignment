a4ea8b2ed7501c6c9e4aa3e590e0b667ae2732b7
Jacob Chapman <chapman.j94@gmail.com>
2014-10-28 22:33:46 +0800

    Moved menu to own function, and moved load (option 1) out of file

--- StudentSearch.java
+++ StudentSearch.java
@@ -13,46 +13,46 @@ public class StudentSearch
 
 	public static void main( String[] args)
 	{
+
+		mainMenu();
+		
+		System.exit(0);
+	}
+
+	private static void mainMenu()
+	{
 		Student[] studentArray = new Student[10000];
-		String inputFile;
-		Scanner input;
 		int selection = 0;
-
+		Scanner input;
 		input = new Scanner(System.in);
-
-		System.out.println("(1)\t - Load Students");
-		System.out.println("(2)\t - Search for Single Student");
-		System.out.println("(3)\t - Search for List of Students");
-		System.out.println("(4)\t - Save Matches");
-		System.out.println("(5)\t - Quit");
 
 		do
 		{
+			System.out.println("(1)\t - Load Students");
+			System.out.println("(2)\t - Search for Single Student");
+			System.out.println("(3)\t - Search for List of Students");
+			System.out.println("(4)\t - Save Matches");
+			System.out.println("(5)\t - Quit");
 			System.out.print("Choice:>");
 			selection = Integer.parseInt(input.nextLine());
 			if( selection < 1 || selection > 5)
 			{
 				System.out.println("Please choose function 1 to 5.");
 			}
-		} while (selection < 1 || selection > 5);
+			switch( selection)
+			{
+				case 1:
+					FileUtils.loadFile( studentArray, input);
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+			}
+		} while (selection != 5);
 
-		switch( selection)
-		{
-			case 1:
-				System.out.print("Enter input file name:>");
-				inputFile = input.nextLine();
-				FileUtils.fillFromFile( studentArray, inputFile);
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-		}
-
-		System.exit(0);
 	}
+
 }
